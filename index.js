ab666822fb11ff045dd3816585dc99811c25a09b
John O'Connor <oconnojb@gmail.com>
2018-09-08 22:42:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ const selectingDrivers = [
   returnLastTwoDrivers
 ]
 const createFareMultiplier = function (int) {return function (fare) {return fare * int }}
+const fareDoubler = function () {}
 
 /*
-+ `createFareMultiplier()` — This is a higher-order function that takes in one argument, an integer,
-and returns a function that will multiply a fare for a ride accordingly.
-If `createFareMultiplier()` receives an argument of `4`,
-it will return a function that takes in a fare as an argument and quadruples the fare.
++ `fareDoubler()` 
+— Declare a variable with `const` and assign a function returned by `createFareMultiplier()` to it. 
+Invoke `createFareMultiplier()` in such a way that the new `fareDoubler()` 
+function accepts a fare as its lone argument and doubles it.
++ `fareTripler()` — Declare a variable with `const` and assign a function returned by `createFareMultiplier()` to it. Invoke `createFareMultiplier()` in such a way that the new `fareTripler()` function accepts a fare as its lone argument and triples it.
++ `fetchSpecifiedDrivers()` — This function takes two arguments, an array of `drivers` and either the `returnFirstTwoDrivers()` or `returnLastTwoDrivers()` function. Based on these two arguments, `fetchSpecifiedDrivers()` will return either the first two drivers or the last two drivers.
 */
