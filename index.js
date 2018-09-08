b7cdec8f812b37b15fe78c95b200fd8e4d9da583
John O'Connor <oconnojb@gmail.com>
2018-09-08 22:36:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 const returnFirstTwoDrivers = function (drivers) { return drivers.slice(0,2)}
 const returnLastTwoDrivers = function (drivers) { return drivers.slice(-2)}
+const selectingDrivers = [
+  returnFirstTwoDrivers,
+  returnLastTwoDrivers
+]
