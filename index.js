ee067fea20b7272f53522229725bf8b93e1fec6f
John O'Connor <oconnojb@gmail.com>
2018-09-08 22:44:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const selectingDrivers = [
   returnLastTwoDrivers
 ]
 const createFareMultiplier = function (int) {return function (fare) {return fare * int }}
-const fareDoubler = function () {}
+const fareDoubler = function (fare) {return createFareMultiplier(2)(fare)}
 
 /*
-+ `fareDoubler()` 
-— Declare a variable with `const` and assign a function returned by `createFareMultiplier()` to it. 
-Invoke `createFareMultiplier()` in such a way that the new `fareDoubler()` 
++ `fareDoubler()`
+— Declare a variable with `const` and assign a function returned by `createFareMultiplier()` to it.
+Invoke `createFareMultiplier()` in such a way that the new `fareDoubler()`
 function accepts a fare as its lone argument and doubles it.
 + `fareTripler()` — Declare a variable with `const` and assign a function returned by `createFareMultiplier()` to it. Invoke `createFareMultiplier()` in such a way that the new `fareTripler()` function accepts a fare as its lone argument and triples it.
 + `fetchSpecifiedDrivers()` — This function takes two arguments, an array of `drivers` and either the `returnFirstTwoDrivers()` or `returnLastTwoDrivers()` function. Based on these two arguments, `fetchSpecifiedDrivers()` will return either the first two drivers or the last two drivers.
